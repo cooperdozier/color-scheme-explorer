43107ad95f067975b14ba1fe80b2c038e56ff285
Cooper Dozier <cooper.dozier@gmail.com>
2020-08-20 09:21:52 -0400

    Updates

---
+++ precache-manifest.df1431d3bd4633a1fa9fbe7e1f0f544f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1abe106838318345cf5ac5882a0b9c",
+    "url": "/color-scheme-explorer/index.html"
+  },
+  {
+    "revision": "2f9d77863655a7936390",
+    "url": "/color-scheme-explorer/static/css/main.c8465cce.chunk.css"
+  },
+  {
+    "revision": "b6fb4d6ee350225d9104",
+    "url": "/color-scheme-explorer/static/js/2.e974b06b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/color-scheme-explorer/static/js/2.e974b06b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9d77863655a7936390",
+    "url": "/color-scheme-explorer/static/js/main.9b1b8f67.chunk.js"
+  },
+  {
+    "revision": "2a8f93a99fe25e31ac30",
+    "url": "/color-scheme-explorer/static/js/runtime-main.a280aaf3.js"
+  },
+  {
+    "revision": "b7c2a1ffd5431853c991447953c19598",
+    "url": "/color-scheme-explorer/static/media/amita-regular-webfont.b7c2a1ff.woff2"
+  },
+  {
+    "revision": "ce7321fa2dc30a73dd6c2536924fce19",
+    "url": "/color-scheme-explorer/static/media/amita-regular-webfont.ce7321fa.woff"
+  },
+  {
+    "revision": "04038e1ba15368260b6537518ca0bdc3",
+    "url": "/color-scheme-explorer/static/media/belligerent-webfont.04038e1b.woff2"
+  },
+  {
+    "revision": "9dc6b7f1c84a76506858e6a69c0b8525",
+    "url": "/color-scheme-explorer/static/media/belligerent-webfont.9dc6b7f1.woff"
+  },
+  {
+    "revision": "604c55af0a257b28d365602bb702e1b5",
+    "url": "/color-scheme-explorer/static/media/comprehension-semibold-webfont.604c55af.woff"
+  },
+  {
+    "revision": "723d889d3303c75a93eab7926ed64991",
+    "url": "/color-scheme-explorer/static/media/comprehension-semibold-webfont.723d889d.woff2"
+  },
+  {
+    "revision": "0d3e2314e3b514d242bdd7bf9d5a48cf",
+    "url": "/color-scheme-explorer/static/media/encodesans-regular-webfont.0d3e2314.woff2"
+  },
+  {
+    "revision": "ddab4e6c6d93b8ef00710b279a3cb066",
+    "url": "/color-scheme-explorer/static/media/encodesans-regular-webfont.ddab4e6c.woff"
+  },
+  {
+    "revision": "22f5338fe84f7310a804b09de39014ac",
+    "url": "/color-scheme-explorer/static/media/overpass-mono-regular-webfont.22f5338f.woff2"
+  },
+  {
+    "revision": "6e4d90e652fbf43bfa57f0bc913ae4e4",
+    "url": "/color-scheme-explorer/static/media/overpass-mono-regular-webfont.6e4d90e6.woff"
+  }
+]);
